27fedb1a9cc7cc10d2d2461428987ad301573863
Roy-999 <77101099+Roy-999@users.noreply.github.com>
2025-10-06 09:50:52 +0530

    Update data_ingestion.py

--- source_files/data_ingestion.py
+++ source_files/data_ingestion.py
@@ -9,20 +9,20 @@ current_dir = Path(__file__).resolve().parent
 secret_file_path = current_dir / ".." / "source_files" / "swapnil.roy@infolob.com_2025-02-28T11_09_10.611Z.pem"
 secret_file_path = secret_file_path.resolve()
 config = {
-    "user": "ocid1.user.oc1..aaaaaaaabepmp6cnh7quc2kq2lp65bg5zoaaafb3i6ysliw4xtsolf5245pq", 
+    "user": user, 
     "key_file": secret_file_path,
-    "tenancy": "ocid1.tenancy.oc1..aaaaaaaamvz6uy5l5mdihzdqex43sp3kwdg45hzhm5djvfiqf2aq2fusbb4q", 
-    "fingerprint": "54:d0:cd:22:ea:f1:2f:ca:01:62:de:58:57:0a:7d:76",
-    "region": "ap-mumbai-1"
+    "tenancy": tenacy, 
+    "fingerprint": fingerprint,
+    "region": region
 }
 
 # Create Object Storage client
 object_storage = oci.object_storage.ObjectStorageClient(config)
 
 # Bucket and namespace details
-namespace = "bmglwagxa8my"  
-bucket_name = "ppepoc_landing"
-folder_prefix = "ascp_cleaned_data/"  # Folder inside the bucket
+namespace = namespace 
+bucket_name = bucket_name
+folder_prefix = folder_prefix  # Folder inside the bucket
 
 # Get list of CSV files in the bucket
 file_list = [
